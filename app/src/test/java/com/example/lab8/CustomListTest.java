616d11d652b67fe400ae04ab9a52e8ef59fddac5
Harjot Singh <harjot1399@gmail.com>
2023-03-10 14:53:37 -0700

    Add city test passed

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,21 +12,23 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
-    private CustomList mockCityList() {
-        CustomList cityList = new CustomList(null,new ArrayList<City>());
-        cityList.add(mockCity());
-        return cityList;
-    }
-    private City mockCity() {
-        return new City("Edmonton", "Alberta");
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
     }
 
     @Test
-    void testAdd() {
-        CustomList cityList = mockCityList();
-        assertEquals(1, cityList.getCount());
-        City city = new City("Regina", "Saskatchewan");
-        cityList.add(city);
-        assertEquals(2, cityList.getCount());
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
     }
+
+
 }
