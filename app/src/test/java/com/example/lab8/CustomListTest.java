4e249d9bc0dbde449b16ce1f239271a8ca5af2a0
Harjot Singh <harjot1399@gmail.com>
2023-03-10 14:47:30 -0700

    ADd city Test Failed

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -6,11 +6,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList mockCityList() {
+        CustomList cityList = new CustomList(null,new ArrayList<City>());
+        cityList.add(mockCity());
+        return cityList;
+    }
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
 
-
+    @Test
+    void testAdd() {
+        CustomList cityList = mockCityList();
+        assertEquals(1, cityList.getCount());
+        City city = new City("Regina", "Saskatchewan");
+        cityList.add(city);
+        assertEquals(2, cityList.getCount());
+    }
 }
